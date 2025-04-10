30e62c9d0da295d33a09709f4686c822778a893b
Steven Pousty <steve.pousty@gmail.com>
2025-04-10 14:51:26 -0700

    setup

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Decisions I need to make
    3. modelresx = foz.load_zoo_model("resnext50-32x4d-imagenet-torch")
 4. Then we are going to run that same model on the Dubai but we need to chip the data first
    https://huggingface.co/docs/transformers/tasks/image_classification
-5. Then do embeddings on the chips\\
+5. Then do embeddings on the chips
 
 5. Then do grounding object detection on dubai data
    https://huggingface.co/docs/transformers/tasks/zero_shot_object_detection
--- test.py
+++ test.py
@@ -1,7 +1,51 @@
 import fiftyone as fo
 import fiftyone.zoo as foz
 
-ds = foz.load_zoo_dataset("quickstart")
-session = fo.launch_app(ds)
+# caltech101_dataset = foz.load_zoo_dataset(
+#     "caltech101",
+#     persistent=True,
+#     overwrite=True,
+#
+# )
+
+dataset = foz.load_zoo_dataset(
+    "caltech256",
+    max_samples=100,
+    shuffle=True
+)
+
+# dataset = foz.load_zoo_dataset(
+#     "coco-2017",
+#     split="validation",
+#     max_samples=200,
+#     shuffle=True,
+#     num_workers=6
+# )
+
+# dataset = foz.load_zoo_dataset(
+#     "open-images-v7",
+#     label_types="classifications",
+#     split="validation",
+#     max_samples=200,
+#     shuffle=True,
+#     num_workers=8
+# )
+
+
+# dataset3 = foz.load_zoo_dataset(
+#     "places",
+#     split="validation",
+#     max_samples=200,
+#     shuffle=True,
+# )
+
+# print("Loading model")
+modelresx = foz.load_zoo_model("resnext50-32x4d-imagenet-torch")
+# # print("running model")
+dataset.apply_model(modelresx, label_field="predictions")
+# print("done with predictions")
+
+# ds = foz.load_zoo_dataset("quickstart")
+session = fo.launch_app(dataset)
 
 session.wait()
